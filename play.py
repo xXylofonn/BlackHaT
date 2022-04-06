b2939011df215aacfa4b234b9646379a43db8d22
xXylofonn <103115634+xXylofonn@users.noreply.github.com>
2022-04-06 12:20:04 +0100

    Add files via upload

---
+++ play.py
@@ -0,0 +1,51 @@
+#GNU nano 6.2               play.py
+#/usr/bin/python
+import time
+from colorama import *
+import  subprocess
+import socket
+import phonenumbers
+print(Fore.BLUE+ "[+]HELLO WORLD")
+subprocess.call('clear',shell=True)
+print(subprocess)
+print ("hello world")
+import time
+time.sleep(3)
+from colorama import *
+print ( "                            PLAY TOOL FOR TERMUX BY CYLOFONN")
+time.sleep(2)
+print (Fore.YELLOW+"Hackers\nAre\nGood\nPeople")
+print ("\n"*3)
+time.sleep(3)
+games = ("1:chess     2:ludo\n3:tetris    4:sudoku")
+print(games)
+quest = input("     Choose game of choice: >>")
+print ("  loading..."*3)
+time.sleep(3)
+print(Fore.RED+".        NOT AVAILABLE NOW. SORRY!")
+print (Fore.GREEN+"Load a hacking tool instead?  ")
+def tools():
+	print(Fore.BLUE+"       1:phoneX   2:ipX\n3:zphisher     4:sqlmap")
+tools()
+print("\n"*3)
+opt = int(input(Fore.RED+"   ENTER NUMBER OF CHOICE >> "))
+if opt == 1:
+	time.sleep(3)
+	print(Fore.GREEN+"launching the process..."*2)
+	print("    Copy and paste the following command\npip install -r requirements1.txt")
+if opt == 2:
+	time.sleep(3)
+	print(Fore.GREEN+"launching the process..."*2)
+	print("    Copy and paste the following command\npip install -r requirements2.txt")	
+if opt == 3:
+	time.sleep(3)
+	print(Fore.GREEN+"launching the process..."*2)
+	print("    Copy and paste the following to install required tools. command below...\npip install -r requirements3.txt")	
+if opt == 4:
+	time.sleep(3)
+	print(Fore.GREEN+"launching the process..."*2)
+	print("    Copy and paste the following command\npip install -r requirements4.txt")	
+	
+
+	
+	
